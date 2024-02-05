2950dd6a236e2a13b4e8ef80a2117afa10e63a40
tahaamin2013 <tahaamindob2013@gmail.com>
2024-02-05 23:21:07 +0500

    sfdsfdfds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           
         {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
